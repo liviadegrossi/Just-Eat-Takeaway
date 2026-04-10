6883f819008c790f6eb1995a2466df6c3edab414
Livia Degrossi <liviadegrossi@gmail.com>
2026-04-10 16:02:08 +0200

    Refactor the endpoint to add pagination and cache storage

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from restaurants.views import index, search_restaurant_by_postcode, search_by_postcode
+from restaurants.views import index, search_by_postcode
 
 urlpatterns = [
     path('', index, name='index'),
-    path('search/', search_restaurant_by_postcode, name='search'),
     path('pagination/', search_by_postcode, name='pagination'),
 ]
